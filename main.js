a8036d5d8877229ad7567cb05d1f08c0b52d20a6
Callmeundoing <77523783+Callmeundoing@users.noreply.github.com>
2021-10-23 17:58:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+song = "";
+
+function preload()
+{
+    song = loudSound("music.mp3");
+}
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristy = 0;
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
